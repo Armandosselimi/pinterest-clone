1e47e1b7d29209e9d55fc92563ce2f7321bdf456
Armando Selimi <armandosselimi@gmail.com>
2025-05-07 17:59:06 +0200

    inital files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,15 @@
+import LeftBar from "components/leftBar";
+import TopBar from "components/TopBar";
 
 function App() {
-
-
   return (
-    <>
-    
-    </>
-  )
+    <div>
+      <LeftBar />
+      <div className='content'>
+        <TopBar />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
