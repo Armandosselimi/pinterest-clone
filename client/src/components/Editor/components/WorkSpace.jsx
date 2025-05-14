ee20f0acfca35635121c2eabc282397aa3abf097
Armando Selimi <armandosselimi@gmail.com>
2025-05-14 17:22:08 +0200

    create pin added. Client side

--- client/src/components/Editor/components/WorkSpace.jsx
+++ client/src/components/Editor/components/WorkSpace.jsx
@@ -1,12 +1,15 @@
 import Image from "components/Image";
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import useEditorStore from "utils/editorStore";
 
 const WorkSpace = ({ prevImg }) => {
-  const { textOptions, setTextOptions, canvasOptions, setCanvasOptions } =
-    useEditorStore();
-
-  const CANVAS_WIDTH = 375;
+  const {
+    textOptions,
+    setTextOptions,
+    canvasOptions,
+    setCanvasOptions,
+    setSelectedLayer,
+  } = useEditorStore();
 
   useEffect(() => {
     if (canvasOptions.height === 0) {
@@ -20,6 +23,41 @@ const WorkSpace = ({ prevImg }) => {
       });
     }
   }, [prevImg, canvasOptions, setCanvasOptions]);
+
+  const itemRef = useRef(null);
+  const containerRef = useRef(null);
+  const dragging = useRef(false);
+  const offset = useRef({ x: 0, y: 0 });
+
+  const CANVAS_WIDTH = 375;
+
+  const handleMouseLeave = (e) => {
+    dragging.current = false;
+  };
+
+  const handleMouseUp = (e) => {
+    dragging.current = false;
+  };
+
+  const handleMouseMove = (e) => {
+    if (!dragging.current) return;
+
+    setTextOptions({
+      ...textOptions,
+      left: e.clientX - offset.current.x,
+      top: e.clientY - offset.current.y,
+    });
+  };
+
+  const handleMouseDown = (e) => {
+    setSelectedLayer("text");
+    dragging.current = true;
+    offset.current = {
+      x: e.clientX - textOptions.left,
+      y: e.clientY - textOptions.top,
+    };
+  };
+
   return (
     <div className='workspace'>
       <div
@@ -28,6 +66,10 @@ const WorkSpace = ({ prevImg }) => {
           height: canvasOptions.height,
           backgroundColor: canvasOptions.backgroundColor,
         }}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUp}
+        onMouseLeave={handleMouseLeave}
+        ref={containerRef}
       >
         <img
           src={prevImg.url}
@@ -41,6 +83,8 @@ const WorkSpace = ({ prevImg }) => {
               top: textOptions.top,
               fontSize: `${textOptions.fontSize}px`,
             }}
+            onMouseDown={handleMouseDown}
+            ref={itemRef}
           >
             <input
               type='text'
